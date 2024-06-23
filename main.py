899520ad5ab59a2e59e213b96a137ca595ca9f25
reverberate <raphaellimfuencoding@gmail.com>
2024-06-23 21:35:06 +0800

    gay

--- main.py
+++ main.py
@@ -1,11 +1,19 @@
 from flask import Flask, render_template
 from models import portfolio
+#Server imports
+from gevent.pywsgi import WSGIServer
+import os
 
-app = Flask(__name__)
+if os.name != "nt":
+    os.chdir(os.path.dirname(_file_))
+
+app = Flask(__name__, template_folder="templates")
 
 @app.route("/")
 def index():
     return render_template('index.html', portfolio = portfolio())
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", debug=True)
+    http_server = WSGIServer(("0.0.0.0", 2003), app)
+    http_server.serve_forever()
+    #app.run(host="0.0.0.0", debug=True)
