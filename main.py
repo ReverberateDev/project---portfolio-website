bd48e2516185388afe8e489454a2693a71c4edb2
Raphael Lim <raphaellim.dev@gmail.com>
2025-02-19 20:46:49 +0800

    Configured main.py for azure cloud hosting

--- main.py
+++ main.py
@@ -14,6 +14,6 @@ def index():
     return render_template('index.html', portfolio = portfolio())
 
 if __name__ == "__main__":
-    http_server = WSGIServer(("0.0.0.0", 2003), app)
-    http_server.serve_forever()
-    #app.run(host="0.0.0.0", debug=True)
+    #http_server = WSGIServer(("0.0.0.0", 2003), app)
+    #http_server.serve_forever()
+    app.run()
