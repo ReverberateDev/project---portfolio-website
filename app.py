d461dced9527d76bc9050c27f26202401a7cb1ce
Raphael Lim <raphaellim.dev@gmail.com>
2025-02-23 16:33:51 +0800

    organised codebase

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from flask import Flask, render_template
-from models import portfolio
+from src.models import portfolio
 #Server imports
 import os
 
--- test.py
+++
@@ -1,4 +0,0 @@
-with open('portfolio.txt', 'r') as portfolio:
-    lines = portfolio.readlines()
-    for line in lines:
-        print(line)
